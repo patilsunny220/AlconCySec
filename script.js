1ab3e1c587d628361c312d37575d74c0c94e071a
DEEP PATIL <70031013+patilsunny220@users.noreply.github.com>
2024-08-28 16:51:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ var init = function(){
     (psScript.addEventListener
       ? psScript.addEventListener('load', init, false)
       : psScript.onload = init);
-    psScript.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/23500/ps-0.9.js';
+    psScript.src = 'particlesliderscript.js';
     psScript.setAttribute('type', 'text/javascript');
     document.body.appendChild(psScript);
   }
